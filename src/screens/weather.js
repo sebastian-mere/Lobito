c79a779ad7097728a62594276de3ae7ff04cb645
Sebastian Mere <sebastian.mere@gmail.com>
2023-03-10 01:15:17 -0300

    modificado el tamaño de la imagen

--- src/screens/weather.js
+++ src/screens/weather.js
@@ -4,11 +4,16 @@ import React from 'react'
 const Weather = () => {
   return (
     <View>
-      <Image source={require("../../assets/lobo0.png")}/>
+      <Image source={require("../../assets/lobo0.png")} style={styles.img} />
     </View>
   )
 }
 
 export default Weather
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    img: {
+        width: 300,
+        height: 300
+    }
+})
