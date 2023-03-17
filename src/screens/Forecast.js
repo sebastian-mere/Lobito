c6c005021525e5adc9b4a41bf8b488086ed87ce4
Sebastian Mere <sebastian.mere@gmail.com>
2023-03-16 21:06:47 -0300

    instalado dependencias y componente forecast

---
+++ src/screens/Forecast.js
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Forecast = () => {
+  return (
+    <View>
+      <Text>Forecast</Text>
+    </View>
+  )
+}
+
+export default Forecast
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    }
+    
+})
